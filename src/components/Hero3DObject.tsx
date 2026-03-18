cc162698eb1f3c8d775ccd6a52f26f7261ea80b1
Jainil1409 <Jainil1vyas1414@gmail.com>
2026-03-18 14:51:03 +0530

    Fix PresentationControls prop types for three drei update

--- src/components/Hero3DObject.tsx
+++ src/components/Hero3DObject.tsx
@@ -13,8 +13,6 @@ export default function Hero3DObject() {
         
         <PresentationControls
           global
-          config={{ mass: 2, tension: 500 }}
-          snap={{ mass: 4, tension: 1500 }}
           rotation={[0.13, 0.1, 0]}
           polar={[-Math.PI / 3, Math.PI / 3]}
           azimuth={[-Math.PI / 1.4, Math.PI / 2]}
